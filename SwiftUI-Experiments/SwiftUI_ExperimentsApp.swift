d7935673ab02da05085a29f58aa962d40ee67799
Theo Vora <theo.vora@gmail.com>
2023-11-11 18:27:37 -0600

    Initial Commit

---
+++ SwiftUI-Experiments/SwiftUI_ExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExperimentsApp.swift
+//  SwiftUI-Experiments
+//
+//  Created by Theo Vora on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
