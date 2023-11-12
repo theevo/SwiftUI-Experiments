005e90bb4ec67c2bb7e7b744daa0cbe1355426f6
Theo Vora <theo.vora@gmail.com>
2023-11-11 18:28:33 -0600

    create button

--- SwiftUI-Experiments/ContentView.swift
+++ SwiftUI-Experiments/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            print("button pressed")
+        }, label: {
+            Text("Button")
+        })
     }
 }
 
