d9ec5ffb00f426abca2013981666c04e8557994c
Theo Vora <theo.vora@gmail.com>
2023-11-11 18:51:02 -0600

    refactor: move animation experiment into its own view file

--- SwiftUI-Experiments/ContentView.swift
+++ SwiftUI-Experiments/ContentView.swift
@@ -8,30 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isVisible = false
-    
     var body: some View {
-        Text("Hello there!")
-            .opacity(isVisible ? 1.0 : 0)
-            .animation(.easeOut(duration: 0.4), value: isVisible)
-        Button(action: {
-            isVisible.toggle()
-        }, label: {
-            Text("Button")
-        })
-        .buttonStyle(GrowingButton())
-    }
-}
-
-struct GrowingButton: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .padding()
-            .background(Color.accentColor)
-            .foregroundStyle(.white)
-            .clipShape(Capsule())
-            .scaleEffect(configuration.isPressed ? 1.2 : 1)
-            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+        Text("Hello world!")
     }
 }
 
